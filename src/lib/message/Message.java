9934c6edfe94b5a9fe3c03deffdaa8df83169eb5
19adrii28 <adrianfrancorodriguez021@gmail.com>
2021-11-10 19:00:24 +0100

    Clase Message Finalizada

---
+++ src/lib/message/Message.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lib.message;
+import java.io.Serializable;
+import lib.dataModel.User;
+/**
+ * Esta clase encapsula el objeto user, el método y el mensaje a enviar.
+ * @author Adrián Franco.
+ */
+public class Message implements Serializable{
+    
+    private User user;
+    private Msg msg;
+
+    public Msg getMsg() {
+        return msg;
+    }
+
+    public void setMsg(Msg msg) {
+        this.msg = msg;
+    }
+
+    public Message() {
+      
+    }
+    
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+    
+    
+}
